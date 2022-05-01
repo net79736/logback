f372f866318c1df15e11e2d19f8bcca88ee84260
leejongwook2 <34103253+leejongwook2@users.noreply.github.com>
2022-05-01 14:26:45 +0900

    Scheduler 기능 추가

--- src/main/java/com/logbackGroup/logback/configuration/DatabaseConfiguration.java
+++ src/main/java/com/logbackGroup/logback/configuration/DatabaseConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -18,6 +19,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.sql.DataSource;
 
 @Configuration
+@ComponentScan("com.logbackGroup.logback")
+@MapperScan
 @EnableTransactionManagement //트랜잭션을 활성화
 public class DatabaseConfiguration {
 
